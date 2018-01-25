503b705488e156969fca22ed92d075755052c0f1
Sara Rose Gallagher <srgallag@gmail.com>
2018-01-25 11:58:31 -0500

    initial

---
+++ src/index.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import registerServiceWorker from './registerServiceWorker';
+import * as firebase from 'firebase'
+
+
+var config = {
+  apiKey: process.env.APIKEY,
+  authDomain: process.env.AUTHDOMAIN,
+  databaseURL: process.env.DATABASEURL,
+  projectId: process.env.PROJECTID,
+  storageBucket: process.env.STORAGEBUCKET,
+  messagingSenderId: process.env.MESSAGINGSENDERID
+}
+
+
+firebase.initializeApp(config);
+
+
+ReactDOM.render(<App />, document.getElementById('root'));
+registerServiceWorker();
